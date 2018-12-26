fb716dd145f0a1d14a770e8d168f96658946a737
thisisaaronland <devnull@localhost>
2018-12-26 07:18:50 -0800

    start blocking out SQL; still doesn't work

--- sqlite.go
+++ sqlite.go
@@ -37,17 +37,24 @@ func NewSQLiteLIFOPool(dsn string) (pool.LIFOPool, error) {
 
 func (pl *SQLiteLIFOPool) Length() int64 {
 
+	sql := fmt.Sprinf("SELECT COUNT(id) FROM %s", pl.Name())
 	// please write me
 	return -1
 }
 
 func (pl *SQLiteLIFOPool) Push(i pool.Item) {
 
+	sql := fmt.Sprintf("INSERT INTO %s ...", pl.Name())
+
 	// error handling/checking?
 	pl.items.IndexRecord(pl.db, i)
 }
 
 func (pl *SQLiteLIFOPool) Pop() (pool.Item, bool) {
+
+	sql := fmt.Sprintf("SELECT MIN(id) FROM %s", pl.Name())
+
+	sql := fmt.Sprintf("DELETE FROM %s WHERE id=?", pl.Name())
 
 	// please write me
 	return nil, false
